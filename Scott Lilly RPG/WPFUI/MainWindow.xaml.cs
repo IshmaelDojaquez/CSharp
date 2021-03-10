893239ceb56b2e30a25ef57c579106dc11e6b330
Ishmael Dojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-03-10 11:11:29 -0500

    Add files via upload

---
+++ Scott Lilly RPG/WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Engine.ViewModels;
+
+namespace WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameSession gameSession;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameSession = new GameSession();
+
+            DataContext = gameSession;
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            gameSession.CurrentPlayer.ExperiencePoints = gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
+    }
+}

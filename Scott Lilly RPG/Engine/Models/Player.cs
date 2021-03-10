3013ff8f3e423246f9b98705eaa6676382d70065
Ishmael Dojaquez <77464884+IshmaelDojaquez@users.noreply.github.com>
2021-03-10 11:13:34 -0500

    Add files via upload

---
+++ Scott Lilly RPG/Engine/Models/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private string name;
+        private string characterClass;
+        private int hitPoints;
+        private int experiencePoints;
+        private int level;
+        private int gold;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string CharacterClass
+        {
+            get { return characterClass; }
+            set
+            {
+                characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+
+        public int HitPoints
+        {
+            get { return hitPoints; }
+            set
+            {
+                hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+
+        public int ExperiencePoints
+        {
+            get { return experiencePoints; }
+            set
+            {
+                experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+
+        public int Level
+        {
+            get { return level; }
+            set
+            {
+                level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+
+        public int Gold
+        {
+            get { return gold; }
+            set
+            {
+                gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
